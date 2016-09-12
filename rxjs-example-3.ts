ad2c9d9c1183a3e37c9ce1e299b9633bf6c41db0
Edward Diehl <EdwDiehl@gmail.com>
2016-09-12 18:46:24 +0300

    Add one more example with rxjs using observable range, map and filter operators

---
+++ rxjs-example-3.ts
@@ -0,0 +1,10 @@
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/range';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/filter';
+
+let sequence:Observable<number> = Observable.range(1, 5)
+    .map((item:number) => item ** 2)
+    .filter((item:number) => item >= 10);
+
+sequence.subscribe((item:number) => console.log(item));
