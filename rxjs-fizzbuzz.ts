63220b95a4f31ac9f26e8e286cea9686dd1c35dc
Edward Diehl <EdwDiehl@gmail.com>
2016-09-13 13:32:53 +0300

    Add delay and repeat to fizzbuzz example

--- rxjs-fizzbuzz.ts
+++ rxjs-fizzbuzz.ts
@@ -1,8 +1,12 @@
 import {Observable} from  "rxjs/Observable";
 import "rxjs/add/observable/range";
+import "rxjs/add/operator/delay"
+import "rxjs/add/operator/repeat"
 import "rxjs/add/operator/map"
 
 let sequence:Observable<void> = Observable.range(1, 100)
+    .delay(1000)
+    .repeat()
     .map((item: number) => {
         let message:string = '';
         
