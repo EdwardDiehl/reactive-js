679af9c70d180ee9af0c216205032a5dcde74844
Edward Diehl <EdwDiehl@gmail.com>
2016-09-13 11:45:31 +0300

    Add fizzbuzz example using rxjs

---
+++ rxjs-fizzbuzz.ts
@@ -0,0 +1,15 @@
+import {Observable} from  "rxjs/Observable";
+import "rxjs/add/observable/range";
+import "rxjs/add/operator/map"
+
+let sequence:Observable<void> = Observable.range(1, 100)
+    .map((item: number) => {
+        let message:string = '';
+        
+        if (item % 3 === 0) message = 'Fizz';
+        if (item % 5 === 0) message += 'Buzz';
+        
+        console.log(message || item);
+    });
+
+sequence.subscribe((item) => item);
