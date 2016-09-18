ca7201dbc77676a40e4ce36d7a73adb02026f370
Edward Diehl <EdwDiehl@gmail.com>
2016-09-18 10:41:42 +0300

    Add onError and onCompleted events callbacks to the 3rd rxjs example

--- rxjs-example-3.ts
+++ rxjs-example-3.ts
@@ -7,4 +7,8 @@ let sequence:Observable<number> = Observable.range(1, 5)
     .map((item:number) => item ** 2)
     .filter((item:number) => item >= 10);
 
-sequence.subscribe((item:number) => console.log(item));
+sequence.subscribe(
+    (item:number) => console.log(item),
+    (error) => console.log('onError: %s', error),
+    () => console.log('onCompleted')
+);
