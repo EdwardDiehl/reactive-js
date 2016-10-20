ad1ba7c5c028cfb18866276603ea00f10d567266
Edward Diehl <EdwDiehl@gmail.com>
2016-10-20 18:19:19 +0300

    Fix type in the example with manageable observable with takeUntil

--- rxjs-example-7.ts
+++ rxjs-example-7.ts
@@ -3,15 +3,16 @@ import {Subject} from 'rxjs/Subject';
 import 'rxjs/add/operator/takeUntil';
 
 let subject:Subject<string> = new Subject();
-let stop:Subject<string> = new Subject();
+let stop:Subject<any> = new Subject();
 
 subject.takeUntil(stop).subscribe(
-    (msg:number) => console.log(msg),
+    (msg:string | number) => console.log(msg),
     (error) => console.log('onError: %s', error),
     () => console.log('onCompleted')
 );
 
 subject.next('Hello');
 subject.next('RxJS');
+subject.next(37);
 stop.next('any message');
 subject.next('subscription stopped and it will be not emitted');
