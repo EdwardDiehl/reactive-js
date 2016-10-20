407bb6ea0e34473169b501a8fbfca50b4ea074f2
Edward Diehl <EdwDiehl@gmail.com>
2016-10-20 18:20:29 +0300

    Add one more example with rxjs using mergeMap

---
+++ rxjs-example-8.ts
@@ -0,0 +1,16 @@
+import {Observable} from "rxjs/Observable";
+import 'rxjs/add/operator/mergeMap';
+import "rxjs/add/observable/of"
+
+let subject:Observable<number> = Observable.of(1, 2, 3, 4, 5).mergeMap((msg:number):Observable<number> => {
+    return Observable.create((s) => {
+        s.next(msg * 10);
+        s.complete()
+    });
+});
+
+subject.subscribe(
+    (msg:number) => console.log(msg),
+    (error) => console.log('onError: %s', error),
+    () => console.log('onCompleted')
+);
