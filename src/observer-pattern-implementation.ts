3645a3b19aa97d668c1277414ac854ce707a30c6
Edward Diehl <EdwDiehl@gmail.com>
2017-06-11 09:19:52 +0300

    Fix codestyle in some examples

--- src/observer-pattern-implementation.ts
+++ src/observer-pattern-implementation.ts
@@ -1,59 +1,59 @@
 interface IListener {
-    update(msg:string):void;
+  update(msg: string): void;
 }
 
 interface IObserver {
-    add(listener:IListener):void;
-    remove(listener:IListener):void;
-    notify(msg:string):void;
+  add(listener: IListener): void;
+  remove(listener: IListener): void;
+  notify(msg: string): void;
 }
 
 class Observer implements IObserver {
-    private _listeners:IListener[] = [];
+  private _listeners: IListener[] = [];
 
-    public add(listener:IListener):void {
-        this._listeners.push(listener)
-    }
+  public add(listener: IListener): void {
+    this._listeners.push(listener);
+  }
 
-    public remove(listener:IListener):void {
-        this._listeners.splice(this._listeners.indexOf(listener), 1)
-    }
+  public remove(listener: IListener): void {
+    this._listeners.splice(this._listeners.indexOf(listener), 1);
+  }
 
-    public notify(msg:string):void {
-        this._listeners.forEach((listener:IListener) => listener.update(msg));
-    }
+  public notify(msg: string): void {
+    this._listeners.forEach((listener: IListener) => listener.update(msg));
+  }
 }
 
-let listener_1 = {
-    update(msg:string){
-        console.log(`Listener_1: ${msg}`);
-    }
+let listener1 = {
+  update(msg: string) {
+    console.log(`Listener1: ${msg}`);
+  }
 };
 
-let listener_2 = {
-    update(msg:string){
-        console.log(`Listener_2: ${msg}`);
-    }
+let listener2 = {
+  update(msg: string) {
+    console.log(`Listener2: ${msg}`);
+  }
 };
 
-let listener_3 = {
-    update(msg:string){
-        console.log(`Listener_3: ${msg}`);
-    }
+let listener3 = {
+  update(msg: string) {
+    console.log(`Listener3: ${msg}`);
+  }
 };
 
-let listener_4 = {
-    update(msg:string){
-        console.log(`Listener_4: ${msg}`);
-    }
+let listener4 = {
+  update(msg: string) {
+    console.log(`Listener4: ${msg}`);
+  }
 };
 
 let observer = new Observer();
 
-observer.add(listener_1);
-observer.add(listener_2);
-observer.add(listener_3);
-observer.remove(listener_1);
-observer.add(listener_4);
+observer.add(listener1);
+observer.add(listener2);
+observer.add(listener3);
+observer.remove(listener1);
+observer.add(listener4);
 
 observer.notify("RxJS + TS => Cool");
